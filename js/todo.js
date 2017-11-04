05c4b418051610abffc560870c5178230d1bed9f
Nick Paolino <github email address>
2017-11-04 11:57:25 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,32 @@
 $(document).ready(function(){
-  // call functions here
+  const form = document.getElementsByTagName('form')[0];
+  createDiv();
+
+  form.addEventListener('submit', function(ev){
+    ev.preventDefault();
+    const inputValue = receiveInput();
+    addListItem(inputValue);
+  })
 });
 
-// define functions here
+function receiveInput(){
+  const item = document.getElementById('item');
+  const value = item.value;
+  item.value = "";
+  return value;
+}
+
+function createDiv(){
+  const div = document.createElement('div');
+  const ul = document.createElement('ul');
+  ul.className = "todo-list";
+  document.body.appendChild(div);
+  div.appendChild(ul);
+}
+
+function addListItem(text){
+  const ul = document.querySelector('.todo-list');
+  const li = document.createElement('li');
+  li.innerText = text;
+  ul.appendChild(li);
+}
